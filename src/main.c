fa5e7f16b87500418f51e99b17a21d8c24dd5f98
Nguyen Phuoc Thanh Lam <phuocthanhlamnguyen@gmail.com>
2026-07-23 11:38:53 +0700

    Added call function

--- src/main.c
+++ src/main.c
@@ -7,12 +7,15 @@ typedef struct Var {
   int Size;
 } Var; // Typedef name
 
+void call(char string[LIMIT_TEST], int Size) {
+  write(1, string, Size);
+}
+
 int main() {
   // Use Variable1 (or struct Variables1)
-  Var writing = {"Testing...testing\n", 19}; 
+  Var writing = {"Testing...testing\n", 19};
   Var *ptr = &writing;
 
-  write(1, ptr->Testing, ptr->Size); 
-  
+  call(ptr->Testing, ptr->Size);
   return 0;
 }
